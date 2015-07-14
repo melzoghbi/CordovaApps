5ac43a83dcde16e69458624b5fc0d937362d42f8
melzoghbi <moelzogh@microsoft.com>
2015-07-14 18:11:59 -0400

    solution for all cordova apps

---
+++ AngularjsCordova/platforms/android/assets/www/controllers/MainCtrl.js
@@ -0,0 +1,17 @@
+﻿
+
+(function () {
+
+    angular.module('myApp', [])
+    .config([function () {
+        // configuration
+    }])
+    .run([function () {
+        // Running
+    }])
+    .controller('MainCtrl', ['$scope', function ($scope) {
+        $scope.myname = "Mostafa Elzoghbi Demo!";
+
+    }])
+
+})();
---
+++ PlayGroundApp/platforms/android/assets/www/scripts/modules/module2/controllers.js
@@ -0,0 +1,4 @@
+﻿module1.controller("Ctrl2", function ($scope) {
+
+    $scope.Test = "This is a test message...";
+});
